4e0fc1edfd639953cffd98050049fbb1c7cb7ee0
aradhya-10 <202051034@iiitvadodara.ac.in>
2023-11-30 15:07:15 +0530

    UC4: Extend the max method

--- FindMax.java
+++ FindMax.java
@@ -1,43 +1,37 @@
+import java.util.Arrays;
+import java.util.Optional;
+
 public class FindMax<T extends Comparable<T>> {
-    private T var1;
-    private T var2;
-    private T var3;
+    private T[] values;
 
-    public FindMax(T var1, T var2, T var3) {
-        this.var1 = var1;
-        this.var2 = var2;
-        this.var3 = var3;
+    public FindMax(T... values) {
+        this.values = values;
     }
 
-    public T testMaximum() {
-        return FindMax.findMax(var1, var2, var3);
+    public Optional<T> testMaximum() {
+        return findMax(values);
     }
 
-    public static <T extends Comparable<T>> T findMax(T var1, T var2, T var3) {
-        T max = var1;
-
-        if (var2.compareTo(max) > 0) {
-            max = var2;
+    public static <T extends Comparable<T>> Optional<T> findMax(T... values) {
+        if (values.length == 0) {
+            return Optional.empty();
         }
 
-        if (var3.compareTo(max) > 0) {
-            max = var3;
-        }
-
-        return max;
+        Arrays.sort(values);
+        return Optional.of(values[values.length - 1]);
     }
 
     public static void main(String[] args) {
-        FindMax<Integer> intFindMax = new FindMax<>(10, 25, 5);
-        Integer maxInteger = intFindMax.testMaximum();
-        System.out.println("The maximum Integer value is: " + maxInteger);
+        FindMax<Integer> intFindMax = new FindMax<>(10, 25, 5, 30, 15);
+        Optional<Integer> maxInteger = intFindMax.testMaximum();
+        maxInteger.ifPresent(value -> System.out.println("The maximum Integer value is: " + value));
 
-        FindMax<Float> floatFindMax = new FindMax<>(10.5f, 25.3f, 5.7f);
-        Float maxFloat = floatFindMax.testMaximum();
-        System.out.println("The maximum Float value is: " + maxFloat);
+        FindMax<Float> floatFindMax = new FindMax<>(10.5f, 25.3f, 5.7f, 15.2f, 30.8f);
+        Optional<Float> maxFloat = floatFindMax.testMaximum();
+        maxFloat.ifPresent(value -> System.out.println("The maximum Float value is: " + value));
 
-        FindMax<String> stringFindMax = new FindMax<>("apple", "banana", "cherry");
-        String maxString = stringFindMax.testMaximum();
-        System.out.println("The lexicographically maximum String is: " + maxString);
+        FindMax<String> stringFindMax = new FindMax<>("apple", "banana", "cherry", "fig", "grape");
+        Optional<String> maxString = stringFindMax.testMaximum();
+        maxString.ifPresent(value -> System.out.println("The lexicographically maximum String is: " + value));
     }
 }
