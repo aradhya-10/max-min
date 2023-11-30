98916d657bd494d408c62b17d429ebd61616a0f0
aradhya-10 <202051034@iiitvadodara.ac.in>
2023-11-30 15:10:36 +0530

    UC5: Print in the max method

--- FindMax.java
+++ FindMax.java
@@ -9,7 +9,15 @@ public class FindMax<T extends Comparable<T>> {
     }
 
     public Optional<T> testMaximum() {
-        return findMax(values);
+        Optional<T> max = findMax(values);
+        if (max.isPresent()) {
+            printMax(max.get());
+        }
+        return max;
+    }
+
+    public static <T> void printMax(T max) {
+        System.out.println("The maximum value is: " + max);
     }
 
     public static <T extends Comparable<T>> Optional<T> findMax(T... values) {
@@ -22,16 +30,16 @@ public class FindMax<T extends Comparable<T>> {
     }
 
     public static void main(String[] args) {
+        // Test case for Integer
         FindMax<Integer> intFindMax = new FindMax<>(10, 25, 5, 30, 15);
         Optional<Integer> maxInteger = intFindMax.testMaximum();
-        maxInteger.ifPresent(value -> System.out.println("The maximum Integer value is: " + value));
 
+        // Test case for Float
         FindMax<Float> floatFindMax = new FindMax<>(10.5f, 25.3f, 5.7f, 15.2f, 30.8f);
         Optional<Float> maxFloat = floatFindMax.testMaximum();
-        maxFloat.ifPresent(value -> System.out.println("The maximum Float value is: " + value));
 
+        // Test case for String
         FindMax<String> stringFindMax = new FindMax<>("apple", "banana", "cherry", "fig", "grape");
         Optional<String> maxString = stringFindMax.testMaximum();
-        maxString.ifPresent(value -> System.out.println("The lexicographically maximum String is: " + value));
     }
 }
